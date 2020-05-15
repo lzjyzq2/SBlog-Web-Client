b6256bd619e357e86a7265e167a555e7b3a586fd
lzjyzq2 <lzjyzq2@qq.com>
2020-05-15 14:18:42 +0800

    fix(api):修复地址拼接问题，增加多个接口方法

--- plugins/sblogclient.js
+++ plugins/sblogclient.js
@@ -1,42 +1,51 @@
 export default function (context, inject) {
     let protocol = process.server ? context.req.protocol : window.location.protocol; //协议
-    let host = process.server ? context.req.protocol : window.location.host; //主机
+    let host = process.server ? context.req.host : window.location.host; //主机
 
     // Create a custom axios instance
     const sblogclient = context.$axios.create();
-    sblogclient.defaults.baseURL = protocol + "//" + host;
+    sblogclient.defaults.baseURL = protocol + "://" + host;
 
     sblogclient.interceptors.request.use(res => {
-        let userinfo = context.store.state.localStorage.userinfo;
-        if (userinfo.token != null || userinfo.token != undefined)
-            res.headers.common['Access-Token'] = userinfo.token || undefined
+        if(process.server){
+            
+        }else{
+            res.headers.common['Access-Token'] = getCookie("token") || undefined;
+        }
         return res;
     });
 
-    let loginPageUrl = '/' + context.store.state.locale + '/login';
-    let indexPageUrl = '/' + context.store.state.locale + '/';
+    // let loginPageUrl = '/' + context.store.state.locale + '/login';
+    // let indexPageUrl = '/' + context.store.state.locale + '/';
+    let loginPageUrl = '/login';
+    let indexPageUrl = '/';
 
-    sblogclient.login = (username, password) => {
+    sblogclient.login = function (username, password){
         return sblogclient.post('api/login', {
             username: username,
             password: password
         }).then(response => {
             if (response.data.code == 20) {
                 context.store.commit('localStorage/setUserInfo', response.data.data);
-                context.redirect(indexPageUrl + response.data.data.uname);
+                //context.redirect(indexPageUrl + response.data.data.uname);
             }
+            return response;
         })
     }
-    sblogclient.register = (username, nickname, password, email, captcha) => {
-        return sblogclient.put('api/register?captcha=' + captcha, {
+
+    sblogclient.register = (username, nickname, password, conpassword, email, captcha) => {
+        return sblogclient.put('api/register', {
             username: username,
             nick: nickname,
             password: password,
-            email: email
-        }).then(res => {
+            conpassword,
+            email: email,
+            captcha
+        }).then((res) => {
             if (res.data.code == 10) {
                 context.redirect(loginPageUrl);
             }
+            return res;
         })
     }
     sblogclient.getRegCaptcha = (username, email) => {
@@ -47,13 +56,48 @@ export default function (context, inject) {
             }
         })
     }
+
+    sblogclient.getBookList = function() {
+        return sblogclient.get('/api/user/book/list');
+    }
+
+    sblogclient.createBook = (name, info) => {
+        return sblogclient.post("/api/user/book/new", { name: name, info: info });
+    }
+
+    sblogclient.updateBook = (id, name, info) => {
+        return sblogclient.post('/api/user/book/update', {
+            id: id,
+            name: name,
+            info, info
+        });
+    }
+
+    sblogclient.getbookInfo = (id) => {
+        return sblogclient.get('/api/user/book/info/' + id);
+    }
+
+    sblogclient.deleteBook = (id) => {
+        return sblogclient.delete('/api/user/book/delete/' + id);
+    }
+
+
+    function getCookie(cname) {
+        var name = cname + "=";
+        var ca = document.cookie.split(';');
+        for (var i = 0; i < ca.length; i++) {
+            var c = ca[i].trim();
+            if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
+        }
+        return "";
+    }
+
     // 添加响应拦截器
     sblogclient.interceptors.response.use(function (response) {
         // 对响应数据做点什么
         return response;
     }, function (error) {
-        // 对响应错误做点什么
-        if (error.data.code == 31) {
+        if (error.response.status == 401) {
             context.redirect(loginPageUrl);
         }
         return Promise.reject(error.response);
