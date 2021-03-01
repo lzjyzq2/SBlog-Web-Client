532c87bf020d07ca94ac354d7e5e59c9086a81af
lzjyzq2 <lzjyzq2@qq.com>
2021-03-01 21:22:06 +0800

    feat(view):新的404错误显示

---
+++ components/error/404.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="page">
+        <div class="statusCode">
+            404 NOT FOUND.
+        </div>
+        <div class="hint">
+            页面被吃掉了！
+        </div>
+        <div>
+            <nuxt-link to="/">返回首页</nuxt-link><a-button type="link"  @click="history.back();">返回上一页</a-button>
+        </div>
+        
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="less" scoped>
+.page{
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-content: center;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+}
+.statusCode{
+  color: black;
+  font-size: 40px;
+}
+.hint{
+    font-size: 30px;
+    color: rgb(87, 220, 238);
+}
+</style>
--- layouts/error.vue
+++ layouts/error.vue
@@ -1,14 +1,25 @@
 <template>
   <div class="container">
-    <h1 v-if="error.statusCode === 404">页面不存在</h1>
-    <h1 v-else>应用发生错误异常</h1>
-    <nuxt-link to="/">首 页</nuxt-link>
+    <page404 v-if="error.statusCode === 404" />
+    <div v-else>
+      应用发生错误异常
+    </div>
   </div>
 </template>
 
 <script>
+import Page404 from "../components/error/404"
+
   export default {
     props: ['error'],
-    layout: 'blog' // 你可以为错误页面指定自定义的布局
+    layout: 'blog', // 你可以为错误页面指定自定义的布局
+    components:{
+      Page404
+    }
   }
 </script>
+<style lang="less" scoped>
+.container{
+  width: 100%;
+}
+</style>
